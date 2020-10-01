2b4cf5a0d5e5d72c4eccba00e3eb1cbfd8e80861
digantshrestha <digantshrestha@gmail.com>
2020-10-01 11:03:32 +0545

    bootstrap

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <?php for($i=1; $i<=10; $i++):?>
-        <h1><?php echo "Hello World"; ?></h1>
-    <?php endfor;?>
+    <div class="container">
+        <?php for($i=1; $i<=10; $i++):?>
+            <h1><?php echo "Hello World"; ?></h1>
+        <?php endfor;?>
+    </div>
 </body>
 </html>
