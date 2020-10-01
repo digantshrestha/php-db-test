3055e0c75d6ea81c03a510c85d7cd676ad44b0d8
digantshrestha <digantshrestha@gmail.com>
2020-10-01 15:53:31 +0545

    postgres connect

--- index.php
+++ index.php
@@ -10,9 +10,31 @@
 </head>
 <body>
     <div class="container">
-        <?php for($i=1; $i<=10; $i++):?>
-            <h1><?php echo "Hello World"; ?></h1>
-        <?php endfor;?>
+        <?php
+            $host = 'host = ec2-54-90-68-208.compute-1.amazonaws.com';
+            $port = 'port = 5432';
+            $dbname = 'dbname = d2j8rpppbp3jmr';
+            $credentials = 'user = thrrzmyfsnrusk password = 2c4291a2107f679675be52483ea51a7150d148a635f90cb8be29dfe892404762';
+
+            $db = pg_connect("$host $port $dbname $credentials");
+
+            // if(!$db){
+            //     echo "Error : Unable to open database\n";
+            // }
+            // else{
+            //     echo "Opened databse successfully\n";
+            // }
+
+            $sql = 'SELECT * FROM courses';
+
+            $query = pg_query($db, $sql);
+
+            while($row = pg_fetch_assoc($query)){
+                echo "<li>".$row['course_name']."</li>";
+            }
+
+            pg_close($db);
+        ?>
     </div>
 </body>
 </html>
